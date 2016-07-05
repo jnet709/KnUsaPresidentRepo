1b8279badbfbb96dff831e3d5c64fb9f0d1bbe2e
jnet709 <jnet709@gmail.com>
2016-07-05 09:15:18 -0500

    First version

---
+++ KnUsaPresident/KnUsaPresident/KnUsaPresident.Droid/obj/Debug/android/src/md54ea250ac66e0bad07400c4ca2d7b052a/MainActivity.java
@@ -0,0 +1,46 @@
+package md54ea250ac66e0bad07400c4ca2d7b052a;
+
+
+public class MainActivity
+	extends md5b60ffeb829f638581ab2bb9b1a7f4f3f.FormsApplicationActivity
+	implements
+		mono.android.IGCUserPeer
+{
+	static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("KnUsaPresident.Droid.MainActivity, KnUsaPresident.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("KnUsaPresident.Droid.MainActivity, KnUsaPresident.Droid, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ KnUsaPresident/KnUsaPresident/KnUsaPresident/KnUsaPresidentPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KnUsaPresident.Models;
+using Xamarin.Forms;
+
+namespace KnUsaPresident
+{
+    public partial class KnUsaPresidentPage : ContentPage
+    {
+        public KnUsaPresidentPage()
+        {
+            InitializeComponent();
+
+            listViewPresidents.ItemsSource = getPresidentList();
+        }
+
+        private IList<President> getPresidentList()
+        {
+            return PresidentService.GetPresidentList();
+        }
+
+        public void Search(object sender, EventArgs e)
+        {
+            var pl = new President();
+            var items = pl.PresidentList;
+            var key = entrySearch.Text;
+
+            items = (nameBehavior.IsChecked
+                        ? items = PresidentService.Search(key, "Name")
+                        : termBehavior.IsChecked ? items = PresidentService.Search(key, "Term") : items = PresidentService.Search(key, "Party")
+                     )
+                     .OrderByDescending(x => x.Id)
+                     .ToList();
+
+            listViewPresidents.ItemsSource = items;
+        }
+    }
+}
+
